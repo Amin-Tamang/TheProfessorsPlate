09ac8fc034b25c53ab03587ddfe1e3e93bcc3998
Amin-Tamang <amin100369@gmail.com>
2025-03-31 09:00:07 +0545

    Project Initialization

---
+++ main/java/com/islington/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.islington.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 23047586 Amin Tamang
+ */
+@WebServlet(asyncSupported = true, urlPatterns = { "/home","/"})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+	}
+
+	
+}
